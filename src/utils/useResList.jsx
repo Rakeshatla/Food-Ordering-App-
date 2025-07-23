c6128c0fb6f4679fc8efbf0742b4f5901405cfe1
Rakeshatla <151203741+Rakeshatla@users.noreply.github.com>
2025-07-23 20:38:07 +0530

    env added

--- src/utils/useResList.jsx
+++ src/utils/useResList.jsx
@@ -7,7 +7,7 @@ const useResList = () => {
     }, [])
     const fetchData = async () => {
         try {
-            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTINGhttps://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            const data = await fetch(import.meta.env.VITE_SWIGGY_API);
             const json = await data.json();
             const restaurantList = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
                 ;
